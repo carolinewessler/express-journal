0e6def34ef43212a1e1c32ac975693ee40dde98c
Caroline Wessler Fois <carol.wessler@hotmail.com>
2025-11-21 16:56:53 -0300

    updates

--- src/middlewares/autenticarExclusive.ts
+++ src/middlewares/autenticarExclusive.ts
@@ -1,9 +1,13 @@
+import type { Request, Response, NextFunction } from 'express';
+
 export function autenticarExclusive(req: Request, res: Response, next: NextFunction) {
-  const user = (req.session as any).user;
+  const user = (req.session as any)?.user;
 
   if (!user || user.role !== 'exclusive') {
-    return res.status(403).send('<h1>Acesso restrito a membros exclusivos</h1><a href="/">Voltar</a>');
+    return res
+      .status(403)
+      .send('<h1>Acesso restrito a membros exclusivos</h1><a href="/">Voltar</a>');
   }
 
-  next(); // se for exclusive, continua
+  next();
 }
